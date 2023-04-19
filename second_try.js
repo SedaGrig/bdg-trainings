3aad666d1192d394c64d71f05c949896a852e1fa
seda <seda@seda.myguest.virtualbox.org>
2023-04-20 03:18:35 +0400

    simple JS function

---
+++ second_try.js
@@ -0,0 +1,9 @@
+// The return statement will return values from a function
+function sayGoodnight1(name) {
+    return 'Goodnight ' + name;
+}
+function sayGoodnight2(name) {
+    return 'Goodnight ' + name;
+}
+console.log(sayGoodnight1());
+console.log(sayGoodnight2('Seda'));`
